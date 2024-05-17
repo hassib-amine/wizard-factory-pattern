b9e411951ee460f77aafc870313c41498676f742
amine hassib <amine.hassib98@gmail.com>
2024-05-17 15:22:23 +0100

    removing the ScreenWizardDTO id

--- src/main/java/com/wizard/demo/dto/WizardScreenDTO.java
+++ src/main/java/com/wizard/demo/dto/WizardScreenDTO.java
@@ -1,14 +1,23 @@
 package com.wizard.demo.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.util.LinkedList;
 import java.util.List;
 @Data
+@EqualsAndHashCode(callSuper = false)
 public class WizardScreenDTO extends BaseDTO{
     private EtablishmentDTO etablishmentDTO;
     private LocationDTO locationDTO;
     private ContactDTO contactDTO;
     private OwnerDTO ownerDTO;
     private List<SpecificationDTO> specificationDTOList = new LinkedList<>();
+
+    @Override
+    @JsonIgnore
+    public Long getId() {
+        return super.getId();
+    }
 }
